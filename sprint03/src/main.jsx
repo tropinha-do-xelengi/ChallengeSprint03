9776233edffd8b3e2ab15381869fd9ad16699dcb
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-03 15:19:14 -0300

    routes

--- sprint03/src/main.jsx
+++ sprint03/src/main.jsx
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Ínicio Bloco de Rotas 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Vistoria from './routes/Vistoria.jsx'
+import Error from './routes/Erro404.jsx'
+
+  const router =  createBrowserRouter([ 
+    {
+      path: "/", 
+      element: <App/>, 
+      errorElement: <Error/>,
+      children:[
+
+      { 
+        path: "/", 
+        element: <Home/>   },
+
+        {      
+          path: "/vistoria", 
+          element: <  Vistoria/>   }
+    ]
+  }
+  ])
+
+
+// Termino Bloco de Rotas
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ sprint03/src/routes/Vistoria.jsx
@@ -0,0 +1,9 @@
+export default function Vistoria() { 
+
+        return (
+            <main>
+                <h1> Vistoria </h1>
+            </main>
+
+        )
+}
