6020828c6668515e1046b7eda16117ba518ccab0
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-06 09:44:53 -0300

    iniciando projetos na home + comecando pelo cabecalho principal

--- sprint03/src/routes/Erro404.jsx
+++ sprint03/src/routes/Erro404.jsx
@@ -3,6 +3,9 @@ export default function Error() {
         return (
             <main> 
                 <h1> Erro 404 - Página não encontrada </h1>
+                    <p> Clique novamente <button> 
+                        Voltar </button> para retornar a página anterior 
+                        </p>
             </main>
         )
 }
