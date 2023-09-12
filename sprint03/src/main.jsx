414c7b0de1b319f4e1594e6f20ae3bae64c80994
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-11 21:06:26 -0300

    aaa

--- sprint03/src/main.jsx
+++ sprint03/src/main.jsx
@@ -4,18 +4,17 @@ import App from './App.jsx'
 import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import EditarProdutos from './routes/EditarProdutos.jsx';
+
 import Erro404 from './routes/Erro404.jsx';
 import Home from './routes/Home.jsx';
-import Produtos from './routes/Produtos.jsx';
+import Vistorias from './routes/Vistoria';
 
 const router = createBrowserRouter([
   { path: '/', element: <App/>,
     errorElement: <Erro404/>,
     children: [
       { path: '/', element: <Home />},
-      { path: '/produtos', element: <Produtos />},
-      { path: '/editar/produtos/:id', element: <EditarProdutos />},
+      { path: '/vistoria', element: <Vistorias />},
     ]
   }
 ]);
